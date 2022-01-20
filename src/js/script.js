5b5a99f4c8f7612a7f2465fa1ed0c80709a001fd
Mr-GoodkaT65 <mr.goodkat65@gmail.com>
2022-01-21 02:04:06 +0330

    header-initial-setup

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,4 @@
-// "use strict";
+"use strict";
 import $ from "jquery";
 
-$(() => {
-  $(".title").on("click", (e) => {
-    console.log(this);
-    $(".title").animate({ "margin-left": "20px" }, 500);
-  });
-});
-
-const title = document.querySelector(".title");
-title.addEventListener("click", function (e) {
-  this.classList.add("title1");
-});
+$(() => {});
